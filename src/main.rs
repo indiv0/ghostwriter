a0807727821e545ce7cf9c00c8a86dbf225c06bf
Nikita Pekin <nikita@frecency.com>
2022-02-02 21:29:31 -0500

    feat: Add code to request completion from goose.ai

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<(), ureq::Error> {
+    // Read the AI API key from the environment.
+    let api_key = match std::env::var("API_KEY") {
+        Ok(v) => v,
+        Err(_) => {
+            eprintln!("set your API_KEY");
+            return Ok(());
+        }
+    };
+
+    // Request a code completion from the AI API.
+    let body = ureq::post("https://api.goose.ai/v1/engines/gpt-neo-20b/completions")
+        .set("Authorization", &format!("Bearer {}", api_key))
+        .send_json(ureq::json!({
+            "prompt": "Say this is a test",
+            "max_tokens": 6,
+        }))?
+        .into_string()?;
+    println!("body: {}", body);
+    Ok(())
 }
